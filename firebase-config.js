58ea606dd9600444472d4f5c01e772fbf37787b9
BrightCodeMaster <147912965+BrightCodeMaster@users.noreply.github.com>
2026-03-03 01:07:17 -0800

    Refactor Firebase config with updated credentials
    Updated Firebase configuration with new API key and app ID.

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,8 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCLVonMIeQcWDgXh8HUf_GxFCnEeu2iw",
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLvNmlEQcWDgXh8HUf_GxFCnEeu2iw",
   authDomain: "lumsign-quote-system.firebaseapp.com",
   projectId: "lumsign-quote-system",
   storageBucket: "lumsign-quote-system.firebasestorage.app",
   messagingSenderId: "1032106770764",
-  appId: "1:1032106770764:web:a075dc48bca51c29f28fe9"
+  appId: "1:1032106770764:web:aa75dc48bca51c29f28fe9"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export { app };
